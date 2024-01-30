2b1758904ce44c783aae842d1e2d8da8a5f470f3
Michael Farrah Girgis <micfar85@gmail.com>
2024-01-30 23:56:07 +0100

    working on layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,35 @@
 import './App.css';
 import {inventory} from "./constants/inventory.js";
-import {productsInStock, productsSold, productsToSell, productDetails, formatPrice, formatSizes} from "./helpers/helpers.jsx";
+import {
+    productsInStock,
+    productsSold,
+    productsToSell,
+    productDetails,
+    formatPrice,
+    formatSizes
+} from "./helpers/helpers.jsx";
 import headerLogo from "./assets/tech_it_easy.png";
 
 function App() {
     return (<>
         <header>
             <div className="logoBox">
-            <img src={headerLogo} alt="Tech it easy logo"/>
+                <img src={headerLogo} alt="Tech it easy logo"/>
             </div>
         </header>
         <main className="body-layout">
             <section className="outer-info-panel">
-                <article className="info-panel">
-                    <h1>{productsInStock(inventory)}</h1>
+                <article className="info-panel-stock">
+                    <p className="info-panel-text">Aantal ingekochte producten</p>
+                    <h1 className="info-panel-textStock">{productsInStock(inventory)}</h1>
                 </article>
-                <article className="info-panel">
-                    <h1>{productsSold(inventory)}</h1>
+                <article className="info-panel-sold">
+                    <p className="info-panel-text">Aantal verkochte producten</p>
+                    <h1 className="info-panel-textSold">{productsSold(inventory)}</h1>
                 </article>
-                <article className="info-panel">
-                    <h1>{productsToSell(inventory)}</h1>
+                <article className="info-panel-toSell">
+                    <p className="info-panel-text">Aantal te verkopen producten</p>
+                    <h1 className="info-panel-textToSell">{productsToSell(inventory)}</h1>
                 </article>
             </section>
             <nav>
